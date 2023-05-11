c8b0d446c4f4cc1fbb99805bb73e3e62594fa05a
beteland123 <91183694+beteland123@users.noreply.github.com>
2023-05-11 18:26:40 +0300

    fix linters on webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,33 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-    mode: 'development',
-    entry:'./src/index.js',
-    devServer: {
-        static: './dist',
+  mode: 'development',
+  entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
       },
-    plugins: [
-        new HtmlWebpackPlugin({
-        template: './src/index.html',
-        }),
-      ],
-    output:{
-        filename:'main.js',
-        path: path.resolve(__dirname,'dist'),
-        clean: true,
-    },
-    module: {
-        rules: [
-          {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-          },
-          {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: 'asset/resource',
-          },
-        ],
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
       },
-    
+    ],
+  },
+
 };
