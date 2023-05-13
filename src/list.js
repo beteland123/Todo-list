acde30ebda1f66d5cc4d452433f7a572e3c497fb
beteland123 <91183694+beteland123@users.noreply.github.com>
2023-05-13 08:32:46 +0300

    edit to js

--- src/list.js
+++ src/list.js
@@ -1,56 +1,64 @@
-// const list = [
-//   {
-//     description: 'wash cloth',
-//     completed: 'true',
-//     index: 3,
-//   },
-//   {
-//     description: 'read book',
-//     completed: 'true',
-//     index: 2,
-//   },
-//   {
-//     description: 'complete webpack project',
-//     completed: 'true',
-//     index: 1,
-//   },
-// ];
-// for (let i = id - 1; i < list.length; i += 1) {
-//    list[i].id = i + 1;
+
+// class lists {
+//    constructor() {
+//      this.lists = JSON.parse(localStorage.getItem('lists')) || [];
+//      this.completed = false;
+//     // this.id = lists.getNextId();
+//    }
+ 
+//    // static getNextId() {
+//    //   return lists.nextId++;
+//    // }
+ 
+//    addlist = (description) => {
+//      const list = {
+//        id: this.lists.length + 1,
+//        description,
+//        completed: false,
+//      };
+//      this.lists.push(list);
+//      localStorage.setItem('lists', JSON.stringify(this.lists));
+//    }
+ 
+//    removelist(id) {
+//     //console.log(id)
+//      this.lists = this.lists.filter((list) => list.id !== parseInt(id, 10));
+//      let localStoragelists = JSON.parse(localStorage.getItem('lists'));
+//      localStoragelists = localStoragelists.filter((obj) => obj.id !== parseInt(id, 10));
+//     //  for (let i = id - 1; i < this.lists.length; i += 1) {
+//     //   console.log(id)
+//     //      this.lists[i].id = i + 1;
+//     //    }
+//      localStorage.setItem('lists', JSON.stringify(localStoragelists));
+//    }
 //  }
+ 
+//  //lists.nextId = 1;
+ 
+//  export default lists;
 class lists {
-   constructor() {
-     this.lists = JSON.parse(localStorage.getItem('lists')) || [];
-     this.completed = false;
-    // this.id = lists.getNextId();
-   }
- 
-   // static getNextId() {
-   //   return lists.nextId++;
-   // }
- 
-   addlist = (description) => {
-     const list = {
-       id: this.lists.length + 1,
-       description,
-       completed: false,
-     };
-     this.lists.push(list);
+  constructor() {
+    this.lists = JSON.parse(localStorage.getItem('lists')) || [];
+   // this.lists = [];
+    this.currentId = 0; // Initialize the currentId property
+  }
+
+  addlist(description) {
+    const task = {
+      id: this.currentId, // Assign a unique ID
+      description,
+    };
+    this.lists.push(task);
+    this.currentId++; // Increment the currentId for the next task
+    
      localStorage.setItem('lists', JSON.stringify(this.lists));
-   }
- 
-   removelist(id) {
-     this.lists = this.lists.filter((list) => list.id !== parseInt(id, 10));
-     let localStoragelists = JSON.parse(localStorage.getItem('lists'));
-     localStoragelists = localStoragelists.filter((obj) => obj.id !== parseInt(id, 10));
-   //   for (let i = id - 1; i < this.lists.length; i += 1) {
-   //       this.lists[i].id = i + 1;
-   //     }
-     localStorage.setItem('lists', JSON.stringify(localStoragelists));
-   }
- }
- 
- //lists.nextId = 1;
- 
- export default lists;
+  }
+
+  removelist(id) {
+    this.lists = this.lists.filter((task) => task.id !== id);
+    localStorage.setItem('lists', JSON.stringify(this.lists));
+  }
+}
+
+export default lists;
  
